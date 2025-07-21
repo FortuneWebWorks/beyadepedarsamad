c58fde67e75564ff62ba572274bb2920b46cc06d
miladkaarandish <miladkaarandish@gmail.com>
2025-07-21 22:38:23 +0330

    fix: trying to make files load

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: "/beyadepedarsamad",
+  assetPrefix: "/beyadepedarsamad/",
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 
+const basePath = "/beyadepedarsamad";
+
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center px-4 py-8 bg-gray-100 text-center space-y-6">
@@ -21,7 +23,7 @@ export default function Home() {
         transition={{ delay: 0.3, duration: 0.8 }}
       >
         <Image
-          src="/picture.jpg"
+          src={`${basePath}/picture.jpg`}
           alt="حاج صدم هاشمی"
           width={300}
           height={300}
@@ -35,7 +37,7 @@ export default function Home() {
         transition={{ delay: 0.5, duration: 0.8 }}
         className="w-full max-w-xs md:max-w-sm"
       >
-        <audio controls src="/voice.m4a" className="w-full" />
+        <audio controls src={`${basePath}/voice.m4a`} className="w-full" />
       </motion.figure>
 
       <motion.blockquote
