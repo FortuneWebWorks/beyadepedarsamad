b176531f73197c7e3fade3a7656b4a70ccc614dc
miladkaarandish <miladkaarandish@gmail.com>
2025-08-04 01:27:28 +0330

    feat: some optimizations

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["drive.google.com"],
+  },
+};
 
 export default nextConfig;
---
+++ src/app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="preconnect" href="https://drive.google.com" />
+      <link rel="dns-prefetch" href="https://drive.google.com" />
+    </>
+  );
+}
